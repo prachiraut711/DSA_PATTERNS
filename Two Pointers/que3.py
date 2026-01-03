144cbef431be954598ae15391c727cc35163c610
prachiraut711 <prachiraut711@gmail.com>
2026-01-03 16:56:02 +0530

    Two Pointers Question Added

---
+++ Two Pointers/que3.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/reverse-string/
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+def reverseString(s):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+    return s
+
+print(reverseString(["h","e","l","l","o"]))
+print(reverseString(["H","a","n","n","a","h"]))
